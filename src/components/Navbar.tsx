aa74049dce18841044f23874cbd7baa655c3c428
Anasbilal2k <anasbilal20202gmail.com>
2024-10-24 17:17:31 -0700

    settings

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,13 +11,13 @@ function Navbar({ className }: { className?: string }): JSX.Element {
     <div className={cn("fixed top-10 inset-x-0 z-50 max-w-md mx-auto bg-gradient-to-r from-black to-gray-800 p-2 py-1 rounded-lg shadow-sm", className)}>
       <Menu setActive={setActive}>
         <MenuItem setActive={setActive} active={active} item="Home">
-          <Link href="/" onClick={() => setActive("Home")} className={`text-white text-base font-medium hover:text-gray-300 transition duration-300 ease-in-out ${active === "Home" ? "text-indigo-800" : ""}`}>
+          <Link href="/" onClick={() => setActive("Home")} className={`text-white text-base font-medium hover:text-gray-300 transition duration-300 ease-in-out ${active === "Home" ? "text-indigo-400" : ""}`}>
             Home
           </Link>
         </MenuItem>
 
         <MenuItem setActive={setActive} active={active} item="Our Courses">
-          <Link href="/courses" onClick={() => setActive("Our Courses")} className={`text-white text-base font-medium hover:text-gray-300 transition duration-300 ease-in-out ${active === "Our Courses" ? "text-indigo-800" : ""}`}>
+          <Link href="/courses" onClick={() => setActive("Our Courses")} className={`text-white text-base font-medium hover:text-gray-300 transition duration-300 ease-in-out ${active === "Our Courses" ? "text-indigo-400" : ""}`}>
             Our Courses
           </Link>
           <div className="mt-1 space-y-1 text-sm text-gray-300">
@@ -28,7 +28,7 @@ function Navbar({ className }: { className?: string }): JSX.Element {
         </MenuItem>
 
         <MenuItem setActive={setActive} active={active} item="Contact">
-          <Link href="/contact" onClick={() => setActive("Contact")} className={`text-white text-base font-medium hover:text-gray-300 transition duration-300 ease-in-out ${active === "Contact" ? "text-indigo-800" : ""}`}>
+          <Link href="/contact" onClick={() => setActive("Contact")} className={`text-white text-base font-medium hover:text-gray-300 transition duration-300 ease-in-out ${active === "Contact" ? "text-indigo-400" : ""}`}>
             Contact
           </Link>
         </MenuItem>
